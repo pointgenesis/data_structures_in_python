07901a0bd063d2516dbeee81ef231ed67692f086
Travis Steinmetz <travissteinmetz@Traviss-MacBook-Pro.local>
2022-01-09 22:53:18 -0500

    chore: optimized the solution... removing unnecessary function

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,10 +1,9 @@
 class Fibonacci:
 
-    def calc(self, number):
-        cache = {}
-        return self.fib_sequence(number, cache)
+    def calc(self, number, cache=None):
+        if cache is None:
+            cache = {}
 
-    def fib_sequence(self, number, cache):
         if number in cache:
             return cache[number]
 
@@ -12,14 +11,14 @@ class Fibonacci:
             cache[number] = 1
             return cache[number]
 
-        cache[number] = self.fib_sequence(number - 1, cache) + self.fib_sequence(number - 2, cache)
+        cache[number] = self.calc(number - 1, cache) + self.calc(number - 2, cache)
 
         return cache[number]
 
 
 def main():
     fib = Fibonacci()
-    print(fib.calc(75))
+    print(fib.calc(50))
 
 
 if __name__ == '__main__':
